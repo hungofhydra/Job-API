7d2b76de387ee267a49bfdaeb65c26f4bff3441b
Nguyen Tan Hung <hungofhydraforwork@gmail.com>
2022-07-19 16:54:11 +0700

    Add Swagger

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 require('express-async-errors');
 const express = require('express');
 const app = express();
+//Security
 const helmet = require('helmet')
 const cors = require('cors')
 const xss = require('xss-clean')
 const rateLimiter = require('express-rate-limit')
-//connect DB
+//Swagger
+const swaggerUI =  require('swagger-ui-express')
+const YAML = require('yamljs')
+const swaggerDoc = YAML.load('./swagger.yaml')
+//Connect DB
 const connectDB = require('./db/connect')
 //routers
 const authRouter = require('./routes/auth')
@@ -16,25 +21,23 @@ const authenticationUser = require('./middleware/authentication')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+//Middleware
 app.set('trust proxy', 1)
 app.use(rateLimiter({
   windowMs : 15*60*1000, //15 minutes
   max : 100
 }));
-
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
 
-// extra packages
 
 // routes
+app.use('/',swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticationUser, jobsRouter)
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
